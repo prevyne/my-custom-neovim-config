a93632b42a9114a1ce89a29c641c55b4e3854788
prevyne <prevyneoketch6@gmail.com>
2026-01-24 20:02:47 +0300

    config: Borrowed Terminal background & foreground color scheme

--- init.lua
+++ init.lua
@@ -10,9 +10,12 @@ vim.cmd("hi Normal ctermbg=NONE guibg=NONE")
 
 -- Tell Neovim to use the terminal's colors for the base editor
 vim.cmd([[
+"This allows the Terminal's oreground and background usable." 
+highlight Normal guifg=NONE ctermfg=NONE guibg=NONE ctermbg=NONE
+
 " For UI elements, you can choose to make them transparent or use terminal colors too.
-  " This makes their background transparent/inherited.
-  highlight SignColumn guibg=NONE ctermbg=NONE
+" This makes their background transparent/inherited.
+highlight SignColumn guibg=NONE ctermbg=NONE
   highlight LineNr guibg=NONE ctermbg=NONE
   highlight CursorLineNr guibg=NONE ctermbg=NONE " Background of the number on the current line
   highlight FoldColumn guibg=NONE ctermbg=NONE
