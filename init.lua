06af03c9361b4fd075334570dabc88d576693901
prevyne <prevyneoketch6@gmail.com>
2026-02-02 02:18:37 +0300

    feat(Background transparency): Configured nvim to override the cyberpunk color scheme background property to {none}

--- init.lua
+++ init.lua
@@ -9,3 +9,6 @@ require("config.cmp")
 --Tell Neovim to use my custom Color Theme
 vim.cmd.colorscheme("hacker_green_ultimate")
 
+--Transparent bg
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })   
